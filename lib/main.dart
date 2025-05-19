8d73db31103abe3c06babc1fe0b02c5373dd508c
HabibieProject <projecthabibie@gmail.com>
2025-05-19 11:29:32 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ppkd_b_2/meet_1.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PPKD B 2',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+      ),
+      home: MeetSatu(),
+    );
+  }
+}
